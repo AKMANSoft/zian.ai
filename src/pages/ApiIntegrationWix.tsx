218c7b532aca80affa1949e2904408176b4a65e1
AKMANSoft <haideraqeel86@hotmail.com>
2023-10-02 18:39:47 +0500

    code cleaning

--- src/pages/ApiIntegrationWix.tsx
+++ src/pages/ApiIntegrationWix.tsx
@@ -5,142 +5,10 @@ import { faCopy } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import useAuthUserStore from "@/lib/zustand/authUserStore";
 import { PrimaryBtn, PrimaryBtnNeon } from "@/components/ui/buttons";
-
-
-const code = `
-
-api_key = 'IST.eyJraWQiOiJxxxxxxxxxxxx0.eyJkYXRhIjoie1wiaWRcIjpcIjkyZTBjNjFhLThjZjMtNDU0YS05NGUzLTdkZjY5Y2I5MDQyNFwiLFwiaWRlbnRpdHlcIjp7XCJ0eXBlXCI6XCJhcHBsaWNhdGlvblwiLFwiaWRcIjpcIjQ1OTliZDQyLTFmZGxxxxxxxx1hZDk2LWQxNjhiMWQ4YzQ2YlwifSxcInRlbmFudFwiOntcInR5cGVcIjpcImFjY291bnRcIixcImlkXCI6XCJlNzU1MzJlMy1kODNkLTRlZTAtYjBmMi1kN2Y2MWI0NzRkYThcIn19IiwiaWF0IjoxNjk1NzM3Nzk0fQ.f4sJQKmS_05BQywhsdDHnSFg-yuKvXg8_J6WdbEh_ZgcTvahDWs2DVCY4sfVe7GrbRt38nXiC0Jb1np7I7L6JGcwq8GX91lbpimvlsgni07uyDuU718jMVL-xTY1610FlnIPXZVSltctVG5oM4Gmsyk6rdaL-9Z3KczS8PZoM5z2vfJHqzjvyl-jW8yCilHF4Yu9eiekdu73dlDNtbagiVGJaoKid5cqpBWjHYkcOvadvKRkcWe9ykp2J8_qMCKroMDhz3vYmJq4-Xz0cXRnfVWIPb8cmzSFQmPZc6YhF9U11XRhWamZe0f-fOz4vJr7Y_XHKbdaSAganDRD5CeV2A'
-site_id = 'xxxxxxxx-d6dc-4ce3-8ff5-d8ac003089fd'
-account_id = 'xxxxxxx-d83d-4ee0-b0f2-d7f61b474da8'
-
-url = 'https://www.wixapis.com/blog/v3/draft-posts'
-img_url = 'https://www.wixapis.com/site-media/v1/files/import'
-member_url = 'https://www.wixapis.com/members/v1/members'
-headers = {'Authorization': api_key, 'wix-account-id': account_id, 'wix-site-id': site_id, 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0'}
-output = inputData
-
-try:
-    r = requests.get(member_url, headers=headers)
-    memberId = r.json()['members'][0]['id']
-
-    nodes = []
-    item = inputData
-    print(f'item id: {item["id"]}')
-    paras = item['article'].split('\n\n')
-
-    for p in paras:
-        nodes.append(
-        {
-            "type": "PARAGRAPH",
-            "id": "",
-            "nodes": [            {
-              "type": "TEXT",
-              "id": "",
-              "nodes": [],
-              "textData":               {
-                "text": p,
-                "decorations": []
-              }
-            }],
-            "paragraphData":             {
-              "textStyle": {"textAlignment": "AUTO"},
-              "indentation": 0
-            }
-         }
-        )
-    img_data = {
-        'url': item['image'],
-        'mimeType': "image/jpeg",
-    }
-    r = requests.post(img_url, headers=headers, json=img_data)
-    try:
-        result = r.json()
-        print(f'image result: {result}')
-    except Exception as e:
-        print(e)
-        print(f'image result: {r.text}')
-        raise e
-
-    #import time
-    #time.sleep(3)
-
-    nodes.insert(0,
-        {
-            "type": "IMAGE",
-            "id": "",
-            "nodes": [
-              {
-                "type": "IMAGE",
-                "id": "",
-                "nodes": [],
-                "imageData": {
-                  'image': {
-                      'src': {
-                          'url': result['file']['url'],
-                          #'url': item['image'],
-                      }
-                  }
-                }
-              }
-            ],
-            "imageData":  {
-              'containerData': {
-                'width': {
-                  'size': 'CONTENT',
-                },
-                'alignment': 'CENTER',
-              },
-              'image': {
-                  'src': {
-                      #'url': result['file']['url'],
-                      'url': item['image'],
-                  }
-              }
-            }
-         }
-    )
-
-    data = {
-        "draftPost": {
-            "title": item['headline'],
-            "richContent": {
-                "nodes": nodes,
-            },
-            'memberId': memberId,
-            "heroImage": {
-                'id': result['file']['id'],
-                'url': result['file']['url'],
-            },
-            'media': {
-                'displayed': False,
-                'custom': True,
-                #'embedMedia': {
-                #    'thumbnail': {
-                #        'url': result['file']['url'],
-                #        #'url': item['image'],
-                #        'width': 800,
-                #        'height': 800,
-                #    }
-                #},
-                'wixMedia': {
-                    'image': {
-                        'id': result['file']['id'],
-                        'url': result['file']['url'],
-                    }
-                }
-            },
-        },
-        "publish": True,
-    }
-    r = requests.post(url, headers=headers, json=data)
-    print(f'status: {r.status_code}')
-    print(f'result: {r.text}')
-except Exception as e:
-    print(e)
+import WixPreCode from "@/components/WixPreCodeComponent";
 
 
 
-`
 
 
 export default function ApiIntegrationWix() {
@@ -395,8 +263,7 @@ export default function ApiIntegrationWix() {
                       </ol>
                     </div>
                   </div>
-
-                  <h2 className="text-base lg:text-[32px] text-white flex gap-2 font-bold font-jakarta leading-[20px] lg:leading-[100%]">
+                  <h2 className="text-base lg:text-[32px] text-white flex gap-1 md:gap-2 font-bold font-jakarta leading-[20px] lg:leading-[100%]">
                     5. <span className="" /> Get your site_id:
                   </h2>
                   <div className="flex flex-col gap-5 pl-6 md:pl-[47px]  md:mt-4">
@@ -467,26 +334,16 @@ export default function ApiIntegrationWix() {
                     </div>
                   </div>
 
-                  <div className="border rounded-xl border-white/10 ">
-
-                    <pre className="px-5 py-4 overflow-x-auto text-xs font-semibold text-white/70 md:text-sm font-jakarta pl-10">
-                      {code}
-                    </pre>
-
-
-
-
-
-                  </div>
+                 <WixPreCode/>
                   <h2 className="text-base lg:text-[32px] text-white flex gap-2 font-bold font-jakarta leading-[20px] lg:leading-[100%]">
                     8. <span className="" /> Run test, check Wix account, then save & turn on Zap. You’re finished! 🎉
 
                   </h2>
-                  <p className=" pl-6 text-base font-normal md:text-xl font-jakarta ">Example :</p>
+                  <p className=" pl-6 md:pl-12 text-base font-normal md:text-xl font-jakarta ">Example :</p>
                   <img
                     src="/images/wix-last-step.png"
                     alt=""
-                    className="w-[512px] h-auto  pl-6"
+                    className="w-[512px] h-auto  pl-6 md:pl-12"
                   />
                 </div>
               </div>
@@ -515,35 +372,4 @@ function VideoComponent() {
   );
 }
 
-// function VideoComponent() {
 
-//     const videoRef = useRef<HTMLVideoElement>(null);
-//     const [showBtn, setShowBtn] = useState(false);
-
-//     useEffect(() => {
-//         videoRef.current?.addEventListener("play", function () {
-//             if (!videoRef.current) return;
-//             setShowBtn(!videoRef.current.paused)
-//         })
-//     }, [videoRef])
-
-//     const onPlayVideoClicked = async () => {
-//         if (!videoRef.current) return;
-//         await videoRef.current.play()
-//         videoRef.current.controls = true;
-//     }
-
-//     return (
-//         <div className="">
-//             <div className="relative w-full md:w-[500px]">
-//                 <video ref={videoRef} src="/videos/test_video.mp4" width={500} height={300} className="object-cover w-full h-auto overflow-hidden rounded-20 aspect-video" />
-//                 <button onClick={onPlayVideoClicked} className={cn(
-//                     "aspect-square p-7 bg-transparent/60 flex items-center justify-center border rounded-full",
-//                     "absolute top-1/2 right-1/2 -translate-y-1/2 translate-x-1/2",
-//                     showBtn && "hidden"
-//                 )}>
-//                     <FontAwesomeIcon className="w-6 h-6 text-lg text-white" icon={faPlay} />
-//                 </button>
-//             </div>
-//         </div>
-//     )}
